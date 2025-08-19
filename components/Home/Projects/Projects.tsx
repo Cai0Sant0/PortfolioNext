ce689e8fbb2603a7cd6f9462593e1fd84bceeda2
Caio Barbosa <caiomontebarbosa104@gmail.com>
2025-08-19 12:39:07 -0300

    aba de skills finalizada

--- components/Home/Projects/Projects.tsx
+++ components/Home/Projects/Projects.tsx
@@ -8,7 +8,7 @@ const Projects = () => {
     return (
         <div className="pt-16 pb-16">
             <h1 className="text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white">
-                Uma pequena seleção de <span className="text-cyan-300">projetos</span> recentes
+                Uma pequena seleção de <span className="text-cyan-300">Projetos</span> recentes
             </h1>
             <div className="w-[70%] mx-auto grid grid-cols-1 md:grid-cols-2 gap-19 mt-16">
 
